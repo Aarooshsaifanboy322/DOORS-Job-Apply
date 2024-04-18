a120698765c8428838f8fa500eb0e71ac7c61b1c
AarooshRBLX <164038746+Aarooshsaifanboy322@users.noreply.github.com>
2024-04-18 19:40:23 +0600

    Update script.js

--- Job Apply/script.js
+++ Job Apply/script.js
@@ -2,5 +2,6 @@ function readytojoin() {
     document.getElementById("enable_apply").disabled = false;
 }
 function link() {
+    window.alert("You are applied to the dev team! But there is one more form left to do.")
     window.open('Final Page/index.html')
 }
